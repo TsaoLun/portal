89516535b5691421bf0273e147e6df01a843112a
tsaolun <ycl18@live.com>
2023-02-07 18:22:51 +0800

    修改：     src/components/pages/home.rs

--- src/components/pages/home.rs
+++ src/components/pages/home.rs
@@ -14,23 +14,30 @@ pub fn home() -> Html {
 
 #[function_component(Form)]
 pub fn form() -> Html {
+    let state = use_state(||"".to_string());
     let (store, dispatch) = use_store::<BoardStore>();
     let onsubmit = dispatch.reduce_mut_callback_with(|state, event: SubmitEvent| {
         event.prevent_default();
         state.data = event.target_unchecked_into::<HtmlInputElement>().value();
-        log!("x")
     });
-    let handle_onchange = Callback::from(move |text: String| {
-        log!(text);
-        text
-    });
+    // let handle_onchange = Callback::from(move |text: String| {
+    //     context = text;
+    // });
+    let handle_onchange = {
+        let state = state.clone();
+        Callback::from(move |x: String|{
+            state.set(x);
+        })
+    };
     html! {
-        <form onsubmit={onsubmit}>
-            <h1>{&store.data}</h1>
-            <TextInput name="copy" handle_onchange={handle_onchange}/>
-            <div>
-                <button>{"Copy"}</button>
-            </div>
-        </form>
+        <ContextProvider<String> context={state.deref().clone()}>
+            <form onsubmit={onsubmit}>
+                <h1>{state.deref()}</h1>
+                <TextInput name="copy" handle_onchange={handle_onchange}/>
+                <div>
+                    <button>{"Copy"}</button>
+                </div>
+            </form>
+        </ContextProvider<String>>
     }
 }
