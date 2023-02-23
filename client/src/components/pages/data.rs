9ec7e5422e12538556d6df8cf64d8362b7be36db
tsaolun <ycl18@live.com>
2023-02-23 13:48:17 +0800

    修改：     client/src/components/pages/data.rs 	修改：     client/src/handlers/data_updater.rs

--- client/src/components/pages/data.rs
+++ client/src/components/pages/data.rs
@@ -1,22 +1,25 @@
 use crate::{
     api::{data, request},
     components::elements::label_input::LabelInput,
-    handlers::data_updater::copy_data,
-    utils::str_tools::cut_to_show,
+    handlers::data_updater::{copy_data, submit_data},
+    utils::str_tools::{cut_to_show, portal},
 };
 use dioxus::{
     events::{FormEvent, MouseEvent},
     prelude::*,
 };
 use dioxus_router::use_router;
-use gloo::{console::*, dialogs::alert};
+use gloo::dialogs::alert;
 
 #[allow(non_snake_case)]
 pub fn Data(cx: Scope) -> Element {
     let router = use_router(cx);
     let state = use_state(cx, || "请输入，或按 C 复制".to_string());
-    let copied_data = use_state(cx, || "".to_string());
-    let init_data = use_state(cx, || "".to_string());
+    let (init_data, copied_data) = (
+        use_state(cx, || "".to_string()),
+        use_state(cx, || "".to_string()),
+    );
+    // init data
     use_effect(cx, (), |_| {
         to_owned![init_data, router];
         async move {
@@ -31,7 +34,8 @@ pub fn Data(cx: Scope) -> Element {
                 }
             }
         }
-    }); // 提供初始化
+    });
+    // watch copied data
     use_effect(cx, copied_data, |_| {
         to_owned![state, copied_data];
         async move {
@@ -44,28 +48,20 @@ pub fn Data(cx: Scope) -> Element {
         e.stop_propagation();
         copy_data(cx, (copied_data.clone(), init_data.clone()), router.clone());
     };
+    let onsubmit = move |e: FormEvent| {
+        submit_data(cx, (state.clone(), e.values["data"].clone()), router.clone());
+    };
+    let ontouchstart = move |_| {
+        copy_data(cx, (copied_data.clone(), init_data.clone()), router.clone());
+    };
+    let ontouchend = move |_| {
+        portal(copied_data.clone().to_string());
+    };
     cx.render(rsx! {
         form {
             oninput: |_| state.set("输入中...".into()),
             onchange: |_| state.set("请记得提交~".into()),
-            onsubmit: move |e: FormEvent| {
-                let state = state.clone();
-                let router = router.clone();
-                cx.spawn(async move {
-                    let res = data::set_mutation(request(), e.values["data"].clone()).await;
-                    match res {
-                        Ok(_data) => {
-                            log!("data submitted!");
-                            state.set("在任意终端按 C 复制".into());
-                        }
-                        Err(err) => {
-                            log!(err.to_string());
-                            alert("登录过期，请重新登录");
-                            router.push_route("/login", None, None);
-                        }
-                    }
-                });
-            },
+            onsubmit: onsubmit,
             prevent_default: "onsubmit",
             style { include_str!("../../assets/data.css") }
             h1 {"{state}"}
@@ -75,6 +71,8 @@ pub fn Data(cx: Scope) -> Element {
             }
             button {
                 onclick: onclick,
+                ontouchstart: ontouchstart,
+                ontouchend: ontouchend,
                 prevent_default: "onclick",
                 class:"copy",
                 "C"
