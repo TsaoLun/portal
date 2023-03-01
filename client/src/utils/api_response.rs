574c3801029780615bb05da6a93b2ffd45b3a835
TsaoLun <ycl18@live.com>
2023-03-02 02:17:51 +0800

    modified:   client/Cargo.lock 	modified:   client/Cargo.toml 	modified:   client/src/utils/api_response.rs

--- client/src/utils/api_response.rs
+++ client/src/utils/api_response.rs
@@ -1,4 +1,5 @@
 use graphql_client::Response;
+use thiserror::Error;
 
 pub struct ErrData {
     pub message: String,
@@ -10,14 +11,27 @@ pub struct ResData<T> {
     pub data: Option<T>,
 }
 
-pub fn get_err<T>(response: Response<T>) -> (String, Option<String>) {
-    let err = response.errors.unwrap();
+pub fn get_err<T>(response: Response<T>) -> Result<(), AppError> {
+    let err = response.errors.map(||);
     let err = err.get(0).unwrap();
     let ext_err = err
         .clone()
         .extensions
         .and_then(|e| e.get("code").map(|code| code.as_str().unwrap().to_string()));
-    (err.clone().message, ext_err)
+    // (err.clone().message, ext_err);
+    todo!()
 }
 
 pub const SERVER_ERROR: &str = "服务端异常";
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("Network error occurred: {0}")]
+    NetworkError(#[from] reqwest::Error),
+    #[error("{0}")]
+    TokenError(String),
+    #[error("{0}")]
+    Other(String),
+}
+
+pub type AppResult<T> = Result<T, AppError>;
