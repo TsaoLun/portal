ce03efc2b4fd121126a316c1f0c04d54e8a0cae6
TsaoLun <ycl18@live.com>
2023-04-13 00:30:51 +0800

    modified:   client/Cargo.lock 	modified:   client/Cargo.toml 	modified:   client/src/api/mod.rs

--- client/src/api/mod.rs
+++ client/src/api/mod.rs
@@ -3,7 +3,14 @@ use gloo::{
     utils::window,
 };
 use lazy_static::lazy_static;
-use reqwest::{Client, RequestBuilder};
+use reqwest::{Client, RequestBuilder, StatusCode};
+use web_sys::Blob;
+use js_sys::{
+    ArrayBuffer,
+    Uint8Array,
+};
+use wasm_bindgen_futures::*;
+use futures::StreamExt;
 
 pub mod data;
 pub mod login;
@@ -25,4 +32,23 @@ pub fn init_url() -> String {
 pub fn request() -> RequestBuilder {
     let token: String = LocalStorage::get("token").unwrap_or_else(|_| "".to_string());
     REQ.post(URL.to_string()).bearer_auth(token)
+}
+
+pub async fn upload_to_server_inner_task(file: web_sys::File) -> Result<StatusCode, anyhow::Error> {
+    let fname = file.name();
+    let blob: Blob = file.into();
+    let blob =
+        Uint8Array::new(
+            JsFuture::from(blob.array_buffer()).await.unwrap().dyn_ref::<ArrayBuffer>().unwrap(),
+        ).to_vec();
+    let client = Client::new();
+    let req =
+        client
+            .post(format!("{}/upload", window().location().origin().unwrap()))
+            .header("Content-Length", &blob.len().to_string())
+            .header("Content-Type", "application/octet-stream")
+            .body(blob)
+            .send()
+            .await?;
+    Ok(req.status())
 }
